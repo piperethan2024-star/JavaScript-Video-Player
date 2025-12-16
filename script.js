093b41ee8f465edc47bb54b7cb2516ba92e9d53d
piperethan2024-star <piper.ethan2024@gmail.com>
2025-12-16 13:51:32 -0600

    adding rest of code

---
+++ script.js
@@ -0,0 +1,58 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const timestamp = document.getElementById('timestamp');
+const progress = document.getElementById('progress');
+
+function playPause() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+function updateIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
+}
+
+function updateProgress() {
+    progress.value = (video.currentTime/video.duration) * 100;
+
+    //get minutes
+    let minutes = Math.floor(video.currentTime / 60);
+    if (minutes < 10) {
+        minutes = '0' + String(minutes);
+    }
+    // get seconds
+    let seconds = Math.floor(video.currentTime %60);
+    if (seconds <10) {
+        seconds = '0' + String(seconds);
+    }
+    timestamp.innerHTML = `${minutes}:${seconds}`;
+}
+
+function setProgress() {
+    video.currentTime = (+progress.value * video.duration) / 100;// add + to turn this from a string into a number
+}
+
+video.addEventListener('click', playPause);
+video.addEventListener('play', updateIcon);
+video.addEventListener('pause', updateIcon);
+video.addEventListener('timeupdate', updateProgress)
+
+play.addEventListener('click', playPause);
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('click', setProgress);
+
+
